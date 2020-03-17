860cd3b0429d8be0c6e01b003bb0fd02930cf385
pocoochan <sayamatsui327@gmail.com>
2020-03-17 19:19:28 +0900

    signUPボタンを押してから登録が完了するまでぐるぐるを表示するようにした

--- date-suggester-iOS/date-suggester-iOS/MainPage/SignUpViewController.swift
+++ date-suggester-iOS/date-suggester-iOS/MainPage/SignUpViewController.swift
@@ -9,55 +9,23 @@
 import UIKit
 
 class SignUpViewController: UIViewController {
-
+    
+    var activityIndicatorView = UIActivityIndicatorView()
+    
     @IBOutlet weak var subView: UIView!
     @IBOutlet weak var myEmail: SignUpCustomTextField!
     @IBOutlet weak var myPassword: SignUpCustomTextField!
     @IBOutlet weak var myPasswordConfirmation: SignUpCustomTextField!
     @IBOutlet weak var signUpButton: UIButton!
     
-    @IBAction func signUpButton(_ sender: Any) {
-    
-        let email = myEmail.text
-        let password = myPassword.text
-        let passwordConfirmation = myPasswordConfirmation.text
-        let signUpParams = [
-            "email": email!,
-            "password": password!,
-            "password_confirmation": passwordConfirmation!
-            ] as [String : Any]
-        
-        let parameter = ["formal_user": signUpParams]
-        
-        // くるくるをだす SVProgressHUD.show()
-        
-        //isUserInteractionEnabledはタッチ可能かどうかをコントロールする
-        self.view.isUserInteractionEnabled = false
-        
-        Api().fomalSignUp(parameter: parameter, completion: {(token, error) in
-            self.view.isUserInteractionEnabled = true
-            //くるくる消す
-            
-            if let _error = error {
-                // アラートを出す
-                return
-            }
-            
-            DispatchQueue.main.async {
-                let defaults = UserDefaults.standard
-                defaults.set(true, forKey: "signUpStatus")
-                
-                let storyboard = UIStoryboard(name: "MainPageViewController", bundle: nil)
-                let mainPageViewController = storyboard.instantiateViewController(withIdentifier: "MainPageViewController")
-                mainPageViewController.modalPresentationStyle = .fullScreen
-                self.present(mainPageViewController, animated: true, completion: nil)
-                debugPrint("サインアップ完了")
-            }
-        })
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        activityIndicatorView.center = view.center
+        activityIndicatorView.style = .whiteLarge
+        activityIndicatorView.color = .darkGray
+
+        
         
         signUpButton.layer.masksToBounds = true
         signUpButton.layer.cornerRadius = 30
@@ -80,6 +48,52 @@ class SignUpViewController: UIViewController {
         }
     }
     
+    @IBAction func signUpButton(_ sender: Any) {
+        let email = myEmail.text
+        let password = myPassword.text
+        let passwordConfirmation = myPasswordConfirmation.text
+        let signUpParams = [
+            "email": email!,
+            "password": password!,
+            "password_confirmation": passwordConfirmation!
+            ] as [String : Any]
+        
+        let parameter = ["formal_user": signUpParams]
+        
+        // くるくるをだす SVProgressHUD.show()
+        view.addSubview(activityIndicatorView)
+        activityIndicatorView.startAnimating()
+        // 非同期処理などが終了したらメインスレッドでアニメーション終了
+        
+        
+        //isUserInteractionEnabledはタッチ可能かどうかをコントロールする
+        self.view.isUserInteractionEnabled = false
+        
+        Api().fomalSignUp(parameter: parameter, completion: {(token, error) in
+            
+            //くるくる消す
+            
+            
+            if let _error = error {
+                // アラートを出す
+                return
+            }
+            
+            DispatchQueue.main.async {
+                let defaults = UserDefaults.standard
+                defaults.set(true, forKey: "signUpStatus")
+                self.view.isUserInteractionEnabled = true
+                self.activityIndicatorView.stopAnimating()
+                
+                let storyboard = UIStoryboard(name: "MainPageViewController", bundle: nil)
+                let mainPageViewController = storyboard.instantiateViewController(withIdentifier: "MainPageViewController")
+                mainPageViewController.modalPresentationStyle = .fullScreen
+                self.present(mainPageViewController, animated: true, completion: nil)
+                debugPrint("サインアップ完了")
+            }
+        })
+    }
+
     @IBAction func backButton(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
