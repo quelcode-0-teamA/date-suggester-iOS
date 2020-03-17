6d1400eeb04aa9aabde11e101b218725498f4e6d
pocoochan <sayamatsui327@gmail.com>
2020-03-17 18:43:07 +0900

    差分をコミット

--- date-suggester-iOS/date-suggester-iOS/MainPage/DateplanListViewController.swift
+++ date-suggester-iOS/date-suggester-iOS/MainPage/DateplanListViewController.swift
@@ -8,20 +8,18 @@
 
 import UIKit
 class DateplanListViewController: UIViewController,UICollectionViewDataSource, UICollectionViewDelegate,  UICollectionViewDelegateFlowLayout{
-
+    
     var resPlanList: [MyPlan]?
     private let itemsPerRow: CGFloat = 2
     
+    @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var popUp: UIButton!
     @IBAction func popUpClose(_ sender: Any) {
-         self.dismiss(animated: true, completion: nil)
+        self.dismiss(animated: true, completion: nil)
     }
-    @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("デートプランリストがでる画面だよ〜")
-        
         DispatchQueue.main.asyncAfter(deadline: .now()+1, execute: {
             let defaults = UserDefaults.standard
             let popUp = defaults.bool(forKey: "popUp")
@@ -57,31 +55,24 @@ class DateplanListViewController: UIViewController,UICollectionViewDataSource, U
         let config: URLSessionConfiguration = URLSessionConfiguration.default
         let session: URLSession = URLSession(configuration: config)
         
-        //URLを組み立てている
         var urlComponents = URLComponents()
         urlComponents.scheme = "https"
         urlComponents.host = "api-date-suggester-dev.herokuapp.com"
         urlComponents.path = "/v1/my_plans"
-
+        
         let url: URL = urlComponents.url!
         var req: URLRequest = URLRequest(url: url)
         req.httpMethod = "GET"
         
-        print(url)
-        
-        //ヘッダーを付与
         let defaults = UserDefaults.standard
         let myToken = defaults.string(forKey: "responseToken")!
         req.setValue("application/json", forHTTPHeaderField: "Content-Type")
         req.setValue("Bearer " + myToken, forHTTPHeaderField: "Authorization")
         
-        //APIを呼ぶよ
         let task = session.dataTask(with: req){(data, response, error) in
             
             do {
                 let response: [[String: Any]] = try JSONSerialization.jsonObject(with: data!, options: []) as! [[String: Any]]
-                
-                print(response)
                 var _myplans = [MyPlan]()
                 for i in response {
                     let myplan = MyPlan(myPlanDicitionary: i)
@@ -92,7 +83,6 @@ class DateplanListViewController: UIViewController,UICollectionViewDataSource, U
                 DispatchQueue.main.async {
                     self.collectionView.reloadData()
                 }
-                
             } catch{
             }
         }
@@ -104,7 +94,7 @@ class DateplanListViewController: UIViewController,UICollectionViewDataSource, U
         DispatchQueue.main.asyncAfter(deadline: .now()+1, execute: {
             let defaults = UserDefaults.standard
             let popUp = defaults.bool(forKey: "popUp")
-
+            
             if popUp {
                 let storyboard = UIStoryboard(name: "MainPageViewController", bundle: nil)
                 let controller = storyboard.instantiateViewController(identifier: "PopupViewController")
@@ -118,15 +108,12 @@ class DateplanListViewController: UIViewController,UICollectionViewDataSource, U
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         //セルの選択を解除
         collectionView.deselectItem(at: indexPath, animated: true)
-
+        
         let my_plan_id = resPlanList?[indexPath.row].id
-        print(my_plan_id)
         let storyboard = UIStoryboard(name: "MainPageViewController", bundle: nil)
         guard let dateScheduleViewController = storyboard.instantiateViewController(withIdentifier: "DateScheduleViewController") as? DateScheduleViewController else {
             return
-        }//これが保証されたら71行めにいく
-        print(my_plan_id)
-        
+        }
         dateScheduleViewController.my_plan_id = my_plan_id
         self.navigationController?.pushViewController(dateScheduleViewController, animated: true)
     }
@@ -139,8 +126,6 @@ class DateplanListViewController: UIViewController,UICollectionViewDataSource, U
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         let datePlanListCell: UICollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "DatePlanListCollectionViewCell", for: indexPath) as! DatePlanListCollectionViewCell
-        
-        
         // Tag番号を使ってインスタンスをつくる
         let photoImageView = datePlanListCell.contentView.viewWithTag(1)  as! UIImageView
         photoImageView.image =  getImageByUrl(url: resPlanList?[indexPath.row].plan.thumb ?? "")
@@ -160,10 +145,12 @@ class DateplanListViewController: UIViewController,UICollectionViewDataSource, U
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
         return UIEdgeInsets(top: 12.0, left: 12.0, bottom: 12.0, right: 12.0)
     }
+    
     //水平方向の余白
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return 12.0
     }
+    
     //垂直方向の余白
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
         return 12.0
